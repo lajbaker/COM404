ee138c71953ab9032de05fe16430e3564ee62568
lajbaker <4bakel36@solent.ac.uk>
2019-10-08 12:43:01 +0000

    Added code to demonstrate counting down with a for loop.

---
+++ 1-Basics/4-repetition/2-for-loop/2-count-down/bot.py
@@ -0,0 +1,5 @@
+print("How far are we from the cave?")
+steps = int(input())
+for count in range(steps,0,-1):
+    print("we are " + str(count) + " steps away from the cave.")
+print("We have arrived at the cave.")
