9add0dd7baa2d022882298e2e67f828a95989612
lajbaker <4bakel36@solent.ac.uk>
2019-10-22 08:50:32 +0000

    included for loop example

--- 1-Basics/6-recap/Q3.py
+++ 1-Basics/6-recap/Q3.py
@@ -1,7 +1,20 @@
+#while loop version
 print("How many zones must I cross?")
 zones = int(input())
 print("Crossing zones...")
+
 while zones > 0:
-    print("crossed zone", zones)
-    zones = zones - 1
+    print("...crossed zone", str(zones))
+    zones -= 1
+
+print("Crossed all zones. Jumanji!")
+
+#for loop version
+print("How many zones must I cross?")
+zones = int(input())
+print("Crossing zones...")
+
+for zones in range(zones, 0, -1):
+    print("...crossed zone", str(zones))
+
 print("Crossed all zones. Jumanji!")
