ea31a3cf9810bddb176fd9efe03023602d4b250b
lajbaker <4bakel36@solent.ac.uk>
2019-10-29 09:40:31 +0000

    beginning function module

---
+++ 1-Basics/7-Mock/q7/functions.py
@@ -0,0 +1,16 @@
+#testing number variable
+number = str(12345)
+
+def option1(number):
+    print("*")
+    print("**")
+    print("***")
+    print("****")
+    print("*****")
+    print("******")
+    print("*******")
+    print("********")
+    print("* " + number + " *")
+    print("**********")
+
+option1(number)
